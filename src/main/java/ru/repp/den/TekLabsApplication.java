7c0931ac4f548b46a809c8d1e018e640300cbb83
Den <reppden@gmail.com>
2016-02-28 15:22:33 +0300

    war build and run

---
+++ src/main/java/ru/repp/den/TekLabsApplication.java
@@ -0,0 +1,21 @@
+package ru.repp.den;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.repp.den.constants.FizzBuzzConstansProvider;
+import ru.repp.den.service.FizzBuzzService;
+import ru.repp.den.service.FizzBuzzServiceImpl;
+
+@SpringBootApplication
+public class TekLabsApplication {
+
+    @Bean
+    public FizzBuzzService fizzBuzzService() {
+        return new FizzBuzzServiceImpl(new FizzBuzzConstansProvider());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekLabsApplication.class);
+    }
+}
